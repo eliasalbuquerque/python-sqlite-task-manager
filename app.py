60ff5c27cc43093b2765ae8afa062f86ff5f943b
Elias Albuquerque <78819295+eliasalbuquerque@users.noreply.github.com>
2024-08-04 14:41:56 -0300

    refactor(app): replaced cache and readline with prompt_toolkit
    ADDED FEATURES:
    
    * replaced readline library with prompt_toolkit
    * implemented prompt_toolkit for prompt
    
    BUG FIXED:
    
    * readline incompatible with cx_freeze to generate executable

--- app.py
+++ app.py
@@ -1,52 +1,53 @@
 # title: 'App TaskPy'
 # author: 'Elias Albuquerque'
-# version: '0.3.0'
+# version: '0.4.0'
 # created: '2024-07-29'
-# update: '2024-08-03'
+# update: '2024-08-04'
 
 
 import os
-import datetime
-from cachetools import LRUCache
+from prompt_toolkit import prompt
 import src.messages as messages
-import src.taskManager as taskmanager
+import src.taskManager as taskManager
 import src.startupMessage as startupMessage
 import src.autoComplete as autoComplete
 
 
-cache = LRUCache(maxsize=100) # Config do cache com tamanho máximo de 100 itens
-cache_file = r'src\cache.json'
-db_file = r'src\tasks.db'
-config_file = r'src\config.json'
-commands = [
-    "--add",
-    "--edit",
-    "--done",
-    "--remove",
-    "--list_done",
-    "--commands",
-    "--undo",
-    "--help",
-    "--message-disable",
-    "--message-enable",
-    "exit",
-]
-
 def clear_terminal():
     os.system('cls' if os.name == 'nt' else 'clear')
+
+def update_autocomplete(auto_complete, task_manager):
+    get_tasks = task_manager.get_list_undone_tasks()
+    auto_complete.update_tasks(get_tasks)
 
 def main():
     """
     Executa o loop principal do aplicativo de gerenciamento de tarefas.
     """
+    
+    db_file = r'src\tasks.db'
+    config_file = r'src\config.json'
+    commands = [
+        "--add",
+        "--edit",
+        "--done",
+        "--remove",
+        "--list_done",
+        "--commands",
+        "--undo",
+        "--help",
+        "--message-disable",
+        "--message-enable",
+        "exit",
+    ]
 
     # Instâncias dos módulos:
-    task_manager = taskmanager.TaskManager(cache, cache_file, db_file)
+    task_manager = taskManager.TaskManager(db_file)
     startup_manager = startupMessage.StartupMessage(config_file)
-    auto_complete = autoComplete.AutoComplete(commands, cache)
 
-    # Configurar auto-complete
-    auto_complete.setup()
+    # Carregar as tarefas do banco no cache de auto-complete
+    get_tasks = task_manager.get_list_undone_tasks()
+    auto_complete = autoComplete.AutoComplete(commands, get_tasks)
 
     # Mostrar mensagem na inicialização
     startup_manager.show_startup_message(task_manager)
@@ -55,7 +56,7 @@ def main():
         while True:
             clear_terminal()
             task_manager.list_undone_tasks()
-            command = input("$ taskpy> ")
+            command = prompt('$ taskpy> ', completer=auto_complete)
 
             if command.strip() == "exit":
                 break
@@ -63,20 +64,28 @@ def main():
             elif " --add" in command:
                 task = command.split(" --add")[0].strip()
                 task_manager.add_task(task)
+                update_autocomplete(auto_complete, task_manager)
 
             elif " --edit " in command:
                 parts = command.split(" --edit ")
                 old_task = parts[0].strip()
                 new_task = parts[1].strip()
                 task_manager.edit_task(old_task, new_task)
+                update_autocomplete(auto_complete, task_manager)
 
             elif " --done" in command:
                 task = command.split(" --done")[0].strip()
                 task_manager.done_task(task)
+                update_autocomplete(auto_complete, task_manager)
 
             elif " --remove" in command:
                 task = command.split(" --remove")[0].strip()
                 task_manager.remove_task(task)
+                update_autocomplete(auto_complete, task_manager)
+
+            elif command == "--undo":
+                task_manager.undo_task()
+                update_autocomplete(auto_complete, task_manager)
 
             elif command == "--list_done":
                 task_manager.list_done_tasks()
@@ -84,9 +93,6 @@ def main():
             elif command == "--commands":
                 print(messages.get_list_of_commands())
                 input("Pressione Enter para continuar... ")
-
-            elif command == "--undo":
-                task_manager.undo_task()
 
             elif command == "--help":
                 print(messages.get_help_message())
@@ -102,7 +108,6 @@ def main():
                 print("Comando inválido. Use --help para ver as opções.")
                 input("Pressione Enter para continuar... ")
     finally:
-        task_manager.save_cache()
         clear_terminal()
 
 if __name__ == "__main__":
